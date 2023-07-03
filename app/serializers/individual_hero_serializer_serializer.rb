091c6616f00f7bce62a23b5eee830529ee6961e8
Job Muturi <muirimuturi17@gmail.com>
2023-07-03 09:47:16 +0300

    Fixed serializers

--- app/serializers/individual_hero_serializer_serializer.rb
+++
@@ -1,4 +0,0 @@
-class IndividualHeroSerializerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :super_name
-
-  has_many :powersend
--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,23 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_03_062803) do
   create_table "hero_powers", force: :cascade do |t|
+    t.string "strength"
+    t.integer "power_id"
+    t.integer "hero_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
